6f45923d7cdf9cb8f3e04b56b81ce380f5f3be35
josue <josue@LAPTOP-I3VRBKPN>
2022-09-23 09:42:18 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora de Propiedades/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Calculadora_de_Propiedades
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.TextBox();
+            this.n2 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 33);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // n3
+            // 
+            this.n3.Location = new System.Drawing.Point(130, 207);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(100, 26);
+            this.n3.TabIndex = 11;
+            // 
+            // n2
+            // 
+            this.n2.Location = new System.Drawing.Point(227, 86);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(100, 26);
+            this.n2.TabIndex = 10;
+            // 
+            // n1
+            // 
+            this.n1.Location = new System.Drawing.Point(30, 86);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(100, 26);
+            this.n1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Numero 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Numero 1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(513, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 32);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(512, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 38);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(512, 244);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 36);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox n3;
+        private System.Windows.Forms.TextBox n2;
+        private System.Windows.Forms.TextBox n1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Calculadora de Propiedades/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_de_Propiedades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Suma s = new Suma();
+            s.V1 = double.Parse(n1.Text);
+            s.V2 = double.Parse(n2.Text);
+
+            n3.Text = s.sumar().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Resta s = new Resta();
+            s.V1 = double.Parse(n1.Text);
+            s.V2 = double.Parse(n2.Text);
+
+            n3.Text = s.Restar().ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Multiplicar s = new Multiplicar();
+            s.V1 = double.Parse(n1.Text);
+            s.V2 = double.Parse(n2.Text);
+
+            n3.Text = s.Multiplica().ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Dividir s = new Dividir();
+            s.V1 = double.Parse(n1.Text);
+            s.V2 = double.Parse(n2.Text);
+
+            n3.Text = s.Dividir().ToString();
+        }
+    }
+}
